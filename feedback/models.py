6a79d28fcc35bb6c9568e83c918b77c2cee0480e
mridu <mridubhatnagar4@gmail.com>
2026-04-03 09:09:47 +0530

    fix: added missing forgein key constraint on the feedback table

--- feedback/models.py
+++ feedback/models.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, String, Text, UniqueConstraint
+from sqlalchemy import Column, DateTime, ForeignKey, String, Text, UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID
 
 from database import Base
@@ -11,8 +11,8 @@ class Feedback(Base):
     __tablename__ = "feedback"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    blog_id = Column(UUID(as_uuid=True), nullable=False)
-    user_id = Column(UUID(as_uuid=True), nullable=False)
+    blog_id = Column(UUID(as_uuid=True), ForeignKey("blog.id"), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("user.user_id"), nullable=False)
     type = Column(String, nullable=False)
     content = Column(Text, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
